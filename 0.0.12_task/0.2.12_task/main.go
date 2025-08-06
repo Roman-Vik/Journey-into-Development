56a09d9952372a341c8d1fdde52ebcacfcf50461
Roman Viktorovich <roman.viktorovich@outlook.com>
2025-08-06 23:45:29 +0300

    #34 task

---
+++ 0.0.12_task/0.2.12_task/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+//Задача 2: Как сделать чтобы первый case выбирался всегда?
+
+func main() {
+	ch1 := make(chan struct{}, 1)
+	ch2 := make(chan struct{}, 1)
+
+	close(ch1)
+	close(ch2)
+	ch1Value := 0.0
+	ch2Value := 0.0
+
+	for i := 0; i < 100000; i++ {
+		select {
+		case <-ch1:
+			ch1Value++
+		default:
+			select {
+			case <-ch2:
+				ch2Value++
+			default:
+			}
+		}
+	}
+
+	fmt.Println(ch1Value / ch2Value) // ожидается 100000 / 0
+}
